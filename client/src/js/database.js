30c7bbce58a3d48c0f02e1af6f887ec38b1fff59
Brice Veyna <bricev99@gmail.com>
2022-08-20 08:46:05 -0700

    Add post and get to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the database');
+  const textEditorDb = await openDB('text', 1);
+  const tx = textEditorDb.transaction('text', 'readwrite');
+  const store = tx.objectStore('text');
+  const request = store.add({ text: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const textEditorDb = await openDB('text', 1);
+  const tx = textEditorDb.transaction('text', 'readonly');
+  const store = tx.objectStore('text');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
