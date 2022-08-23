18b624352ae5f8f822a4d885f09bfd38935b3f4b
Brice Veyna <bricev99@gmail.com>
2022-08-22 22:22:38 -0700

    fix formatting, errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,19 +14,19 @@ const initdb = async () =>
 
 export const putDb = async (content) => {
   console.log('Post to the database');
-  const textEditorDb = await openDB('text', 1);
-  const tx = textEditorDb.transaction('text', 'readwrite');
-  const store = tx.objectStore('text');
-  const request = store.add({ text: content });
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
 export const getDb = async () => {
   console.log('GET from the database');
-  const textEditorDb = await openDB('text', 1);
-  const tx = textEditorDb.transaction('text', 'readonly');
-  const store = tx.objectStore('text');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
